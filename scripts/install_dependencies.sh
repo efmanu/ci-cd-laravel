9e1dad9d3e163601525364100118d4ba2c055f55
Manu <manuedavakandam@gmail.com>
2025-01-05 22:29:48 +1100

    debug

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 set -e
 
-echo "Configure repo"
+echo "debug"
 # apt-get install -y software-properties-common
 # add-apt-repository -y ppa:ondrej/php
 
 # # Update package lists and install dependencies
-# echo "Updating ubuntu"
 # apt-get update
-
-# echo "Installing libraries"
 # apt-get install -y apache2 php7.4 php7.4-cli php7.4-common php7.4-mysql php7.4-zip php7.4-gd php7.4-mbstring php7.4-curl php7.4-xml php7.4-bcmath php7.4-pdo nano vim
 # # Enable PHP module and restart Apache
 # a2enmod php7.4
