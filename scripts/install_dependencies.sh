fcf2efe6b27ed4428a52df3f0dcc974f4daeb15e
Manu Francis <manuedavakandam@gmail.com>
2025-01-05 21:48:53 +1100

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository -y ppa:ondrej/php
+
 # Update package lists and install dependencies
 apt-get update
 apt-get install -y apache2 php7.4 php7.4-cli php7.4-common php7.4-mysql php7.4-zip php7.4-gd php7.4-mbstring php7.4-curl php7.4-xml php7.4-bcmath php7.4-pdo nano vim
